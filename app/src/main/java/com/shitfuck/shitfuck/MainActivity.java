5161c2175724a1bb7330b76c3178062f4fa1502f
acettea8 <acettea8@gmail.com>
2017-02-13 19:55:55 +0100

    g rajouté un booléen fieu

--- app/src/main/java/com/shitfuck/shitfuck/MainActivity.java
+++ app/src/main/java/com/shitfuck/shitfuck/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int iii=9;
+        boolean bool = true;
     }
 }
